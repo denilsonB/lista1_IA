778082704de9c38820914d3823a44ca06d749a05
denilsonB <33883607+denilsonB@users.noreply.github.com>
2022-09-29 10:03:45 -0300

    refactoring

--- q1/questao_1.py
+++ q1/questao_1.py
@@ -1,45 +1,35 @@
 #Denilson Bulhões da Rosa Silva
-#José Arthur Lopes
+#José Arthur Lopes Sabino
 class Expressao: #classe para representar a exporessão, ex: A = SIM
 
     def __init__(self, var, opr, val):
         self.var = var
         self.opr = opr
         self.val = val
-
+    
     def __str__(self):
-        return self.var
+        return self.variavel()
 
     def variavel(self):
         if self.val == True:
             return self.var
         return "~" + self.var
-    
-    def valor(self):
-        if self.val == True:
-            return "SIM"
-        else:
-            return "NÃO"
 
     def comparar(self,outra_expressao):
-        if(self.var == outra_expressao.var) :
+        if (self.var == outra_expressao.var) and (self.opr == outra_expressao.opr) and (self.val==outra_expressao.val):
             return True
-        else:
-            return False
-    def comparar_exatidao(self,outra_expressao):
-        if(self.var == outra_expressao.var) and (self.opr == outra_expressao.opr) and (self.val == outra_expressao.val):
-            return True
-        else:
-            return False
+        return False
+
 base_de_regras = []
 base_de_fatos = []
 objetivo = Expressao("","",True)
 
 def recebe_regras(qtd):
-    print("as expressões separadas por &\n ex: A & B\n negações com o simbolo de ~ ex: ~A")
+    print("as expressões separadas por &\n ex: A & B")
     for i in range(qtd):
         print("digite o(s) precedente(s) da {} regra".format(i + 1))
         antecedente = input("").split("&")
+
         expressoes_antecedentes = []
 
         for var in antecedente:# constroi os antecedentes
@@ -76,7 +66,6 @@ def recebe_fatos(qtd):
         if var[0] == '~':
             exp.var = var[1:]
             exp.val = False
-            modus_tollens(exp)
         base_de_fatos.append(exp)
 
 def e_fato(elemento):
@@ -92,11 +81,7 @@ def remove_regra_da_base(uma_regra):
             break
 
 def valida_regra(uma_regra):
-    fatos_antecedentes = '&'.join(exp.var for exp in uma_regra[0])
-    exp = Expressao(fatos_antecedentes,"=",True)    
-    if e_fato(exp):
-        return True
-    
+
     premissas = 0
     for antecedente in uma_regra[0]:
         if e_fato(antecedente) :
@@ -109,38 +94,10 @@ def valida_regra(uma_regra):
                 return True
     return False
 
-def modus_tollens(um_fato):# ta errado, diz que é sim msm sem ser por algum motivo, deve ser culpa daqui
-    #percorre a base de regras
-    #verifico se meu fato é o consequente de alguma
-    #nego o antecedente e o adiciono na base de regras
-    for i in range(len(base_de_regras)):
-        fatos_antecedentes = '&'.join(exp.var for exp in base_de_regras[i][1])
-        exp = Expressao(fatos_antecedentes,"=",False)        
-        if um_fato.comparar_exatidao(exp):
-            fatos_antecedentes = '&'.join(exp.var for exp in base_de_regras[i][0])
-            exp = Expressao(fatos_antecedentes,"=",False)
-            if e_fato(exp) == False:
-                base_de_fatos.append(exp)
-
-def silogismo_hipotetico():
-    for i in range(len(base_de_regras)):
-        consequente = base_de_regras[i][1][0]
-        for j in range(len(base_de_regras)):
-            if(len(base_de_regras[j][0]) > 1 ):
-                continue
-            fato_antecedente = base_de_regras[j][0][0]
-            if (consequente.comparar_exatidao(fato_antecedente)):
-                antecedente_regra = base_de_regras[i][0]
-                consequente_regra = base_de_regras[j][1]
-                regra = []
-                regra.append(antecedente_regra)
-                regra.append(consequente_regra)
-                base_de_regras.append(regra)
 
 def estabelecer_um_fato(meta):
     if e_fato(meta) :
         return True
-    #base_de_regras.sort(key=lambda e: len(e[0]))
     copia_das_regras = base_de_regras.copy()
     executar_um_ciclo(copia_das_regras)
 
@@ -150,43 +107,48 @@ def executar_um_ciclo(as_regras):
 
     uma_regra = as_regras.pop(0)
     if valida_regra(uma_regra):
-        if objetivo.comparar_exatidao(uma_regra[1][0]):
+        if objetivo.comparar(uma_regra[1][0]):
             base_de_fatos.append(objetivo)
             return True
         if (e_fato(uma_regra[1][0]) == False):#verifica se o consequente não está na base de fatos
+                fatos_antecedentes = '&'.join(exp.var for exp in uma_regra[0])
                 base_de_fatos.append(uma_regra[1][0])
-                if(uma_regra[1][0].val == False):
-                    modus_tollens(uma_regra[1][0])
                 
         remove_regra_da_base(uma_regra)
         executar_um_ciclo(base_de_regras)
         return
     executar_um_ciclo(as_regras)
 
-def valida_objetivo():
-    for exp in base_de_fatos:
-        if objetivo.comparar_exatidao(exp):
-            return True
-    return False
+def silogismo_hipotetico():
+    for i in range(len(base_de_regras)):
+        consequente = base_de_regras[i][1][0]
+        for j in range(len(base_de_regras)):
+            if(len(base_de_regras[j][0]) > 1 ):
+                continue
+            fato_antecedente = base_de_regras[j][0][0]
+            if (consequente.comparar(fato_antecedente)):
+                antecedente_regra = base_de_regras[i][0]
+                consequente_regra = base_de_regras[j][1]
+                regra = []
+                regra.append(antecedente_regra)
+                regra.append(consequente_regra)
+                base_de_regras.append(regra)
+
+
+def refazer_base(base_original):
+    for regra in base_original:
+        base_de_regras.append(regra)
 
 def mostar_fatos():
     print("\nEsta é a base de fatos:")
     for i in base_de_fatos:
          print("fato ",i)
 
-def mostar_regras(base_original):
-    print("\nEstá é a base de regras:")
-    for i in range(len(base_original)):
-        fatos_antecedentes = '&'.join(exp.var for exp in base_original[i][0])
-        fato_consequente = base_original[i][1][0]
-        if base_original[i][0][0].val == False:
-            fatos_antecedentes = '~' + fatos_antecedentes
-        if fato_consequente.val == False:
-            fato_consequente.var = '~' + fato_consequente.var
-        print(fatos_antecedentes,"-->",base_original[i][1][0].var)
-def refazer_base(base_original):
-    for regra in base_original:
-        base_de_regras.append(regra)
+def resolveu():
+    for fato in base_de_fatos:
+        if(objetivo.comparar(fato)):
+            return True
+    return False
 
 def main():
     print("Sistema baseados em conhecimento.\nResolvedor de sentenças no formato SE expressão ENTÃO expressão")
@@ -204,18 +166,15 @@ def main():
     objetivo.var = obj 
     objetivo.opr = "="
     objetivo.val = True
-    if obj[0] == '~':
-        objetivo.var = obj[1:]
-        objetivo.val = False
+    
     
     estabelecer_um_fato(objetivo)
-    if valida_objetivo == False:
+    if(resolveu() == False):
         refazer_base(base_original)
-        estabelecer_um_fato
-    mostar_regras(base_original)
+        estabelecer_um_fato(objetivo)
     mostar_fatos()
-    if valida_objetivo():
-        print("\nEstá provado",objetivo)
+    if resolveu():
+        print("está provador que ",objetivo) 
     else:
-        print("\nÉ falso que ",objetivo)
+        print("Falso")
 main()
